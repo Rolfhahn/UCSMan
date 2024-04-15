46e9976902f6dcc4c8f43d6a7d0aa68cf8479ba4
Rolfhahn <Rolf.hahn@microdyn.ch>
2024-04-15 19:03:20 +0200

    Update UnifiControllerasWindowsServiceSetupManager.bat
    - Updated Information about Java 17 Eclipse Temurin OpenJRE compatibility with UniFi Network Application 8.1.113
    - Updated Information about JAVA_HOME Variable

--- UnifiControllerasWindowsServiceSetupManager.bat
+++ UnifiControllerasWindowsServiceSetupManager.bat
@@ -1,7 +1,10 @@
 cls
 @echo off
-echo UCSMan - UniFi Controller as Windows Service Manager    Ver.: 1.2 / 2021-04-26
+echo MD UCSMan - UniFi Network Application as Win. Service Manager V.1.4/2024-04-15
 echo ------------------------------------------------------------------------------
+echo                               UniFi and UBIQUITI are Ubiquiti Inc Brands
+echo                               Batch Autor: Rolf Hahn     rolf.hahn@microdyn.ch
+echo.
 echo This procedure installs / removes UniFi Controller as Windows Service:
 echo   We use the following commands to install the service:
 echo     java -jar lib\ace.jar installsvc
@@ -10,20 +13,17 @@ echo   We use the following commands to remove the service:
 echo     java -jar lib\ace.jar stopsvc
 echo     java -jar lib\ace.jar uninstallsvc
 echo.
-echo                                    UniFi and UBIQUITI are Ubiquiti Inc Brands
-echo                                    Copyright of this Batch: Rolf Hahn
-echo                                                         rolf.hahn@bluewin.ch
-echo.
-echo       Before you use this script make sure you have this prerequisites:
-echo       - Windows 10 (current Version)
-echo       - UniFi Network Controller=6.2.17+ f. Wind. 
-echo         from: https://dl.ui.com/unifi/6.2.17-ceb3ba714d/UniFi-installer.exe
-echo       - Only JAVA 8.281 64Bit + is neccesary. From: https://java.com/de/
-echo ------------------------------------------------------------------------------
-echo Important:
-echo Please start this batch with a user that has local administrator privileges.
-echo.
-echo You can stop this script by pressing [CTRL-X] or [q] then [ENTER]
+echo  Requirements to successful use this script:
+echo       - Windows 10+ (current Version) We have tested: 10.0.19045.9291
+echo       - JAVA 17 Eclipse Temurin OpenJRE is neccesary from:
+echo         https://adoptium.net/de/temurin/releases/?os=windows&arch=x64&package
+echo         =jre&version=17
+echo       - UniFi Network Application 8.1.113 for Windows
+echo         from: https://www.ui.com/download/unifi
+echo         Important: Set JAVA_HOME variable while installing java
+echo       - Start this batch with local administrator privileges
+echo  please read also: https://help.ui.com/hc/en-us/articles/205144550-Self-
+echo         Hosting-a-UniFi-Network-Server-as-a-Windows-Service-Advanced
 echo.
 :ProgramStart
 echo Please select Batch-Mode: 
@@ -38,9 +38,12 @@ goto ProgramStart
  echo.
  echo Controller as Service will be installed now... 
  cd "%userprofile%\ubiquiti unifi\"
- java -jar lib\ace.jar installsvc
- java -jar lib\ace.jar startsvc
- echo done
+ java.exe -jar lib\ace.jar installsvc
+ java.exe -jar lib\ace.jar startsvc
+ echo done.
+ echo You can now navigate to:
+ echo   - https://localhost:8443/manage                 - to manage the wifi network
+ echo   - https://localhost:8443/manage/hotspot-manager - to manage your Guests
  echo.
 goto ProgramEnd
 
@@ -48,8 +51,8 @@ goto ProgramEnd
  echo.
  echo Controller as Service will be removed now... 
  cd "%userprofile%\ubiquiti unifi\"
- java -jar lib\ace.jar stopsvc
- java -jar lib\ace.jar uninstallsvc
+ java.exe -jar lib\ace.jar stopsvc
+ java.exe -jar lib\ace.jar uninstallsvc
  echo done
  echo.
 goto ProgramEnd
